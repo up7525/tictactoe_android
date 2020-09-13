42c2e040b8378ed457b7ed9bc2b4ce47bce17f4f
Valentin Mairesse <mairesse.valentin.pro@gmail.com>
2020-09-13 19:23:52 +0200

    ecouteBouton working

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    String turn = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +21,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
-
+        ecouteBouton();
     }
 
-    private void ecouteBouton(Button btn) {
-        btn.setOnClickListener(new Button.OnClickListener(){
-            public void onClick(View v) {
-            //    btn.setText("lol");
-            }
-        });
+    private void ecouteBouton() {
+        ArrayList<Button> listeBoutons = new ArrayList<Button>();
+
+        listeBoutons.add(((Button) findViewById(R.id.button)));
+        listeBoutons.add(((Button) findViewById(R.id.button2)));
+        listeBoutons.add(((Button) findViewById(R.id.button3)));
+        listeBoutons.add(((Button) findViewById(R.id.button4)));
+        listeBoutons.add(((Button) findViewById(R.id.button5)));
+        listeBoutons.add(((Button) findViewById(R.id.button6)));
+        listeBoutons.add(((Button) findViewById(R.id.button7)));
+        listeBoutons.add(((Button) findViewById(R.id.button8)));
+        listeBoutons.add(((Button) findViewById(R.id.button9)));
+
+        for (final Button value : listeBoutons) {
+            value.setOnClickListener(new Button.OnClickListener(){
+                public void onClick(View v) {
+                    value.setTextColor(Color.BLUE);
+                    value.setText("X");
+                }
+            });
+        }
+
     }
 
     private String winner(String gg) {
